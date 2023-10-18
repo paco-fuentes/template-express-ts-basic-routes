4f10fd30a14ba2c518f277f27254fbea959c0ac1
Paco Fuentes <pacofuentes.work@gmail.com>
2023-10-18 21:04:24 +0200

    feat: created a get, post, put and delete estructure on index.tx file

--- README.md
+++ README.md
@@ -2,5 +2,8 @@
 Template for backend projects with Node and Express and TypeScript basic routes defined.
 
 1. Install npm depdencies with "$ npm install" command
-2. Command "$ npm run dev" to execute index.ts and run the localhot server at port 4000, you should see this in your console: "hello world from port 4000 message" and acces [localhost:4000](http://localhost:4000/) in your browser.
+2. Command "$ npm run dev" to execute index.ts and run the localhot server at port 4000, you should see this in your console: "hello world from port 4000 message" and access [localhost:4000](http://localhost:4000/) in your browser.
 3. Command "$ npm build" to compile your index.ts file onto ./build directory.
+4. Command "$ npm run start" to launch the localhost server with compiled ./build/index.js
+
+Press 'ctr' + 'c' onto your terminal to stop server running.
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,46 @@
 import express from "express";
 const app = express();
 
+// get
+app.get('/run-test', (req, res) => {
+    // lógica para hacer y devolver
+    return res.send('GET THINGS');
+});
 
+// post
+app.post('/run-test', (req, res) => {
+    // lógica para crear, test
+    return res.send('POST-CREATE THINGS');
+});
+
+// put con /:id desde postman recuperamos el /754765 en nuestra consola
+app.put('/run-test/:id', (req, res) => {
+    // lógica para actualizar películas
+
+    // con el metodo de req, params, recuperamos el id de /:id
+    console.log(req.params);
+
+    //lo guardamos en una variable
+    const testId = req.params.id;
+    return res.send('UPDATE TEST CON EL ID ' + testId);
+});
+
+// // put
+// app.put('/run-test', (req, res) => {
+//     // logica para actualizar peliculas
+//     return res.send('UPDATE FILMS');
+// });
+
+// delete
+app.delete('/run-test/:id', (req, res) => {
+    // logica para borrar test
+
+    //lo guardamos en una variable
+    const testId = req.params.id;
+    return res.send('DELETE TEST CON EL ID: ' + testId);
+});
+
+// server running on port ...
 app.listen(4000, () => {
-    console.log("Hola Mundo! desde el localhost 4000");
+    console.log("Server Running: ¡Hola mundo from localhost 4000!");
 });
